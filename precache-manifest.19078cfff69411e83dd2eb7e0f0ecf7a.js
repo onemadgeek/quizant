b124fde289cc4068a97134cc70b1edebc4b090ec
nurA <arun@honeybadgerlabs.in>
2019-08-18 16:15:28 +0530

    Updates

---
+++ precache-manifest.19078cfff69411e83dd2eb7e0f0ecf7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc47d01ab8dfb00a1857ce48b49c9934",
+    "url": "/quizant/index.html"
+  },
+  {
+    "revision": "f056e6abfd6c65575171",
+    "url": "/quizant/static/css/main.35f7f619.chunk.css"
+  },
+  {
+    "revision": "6ed9c62ccbd34a0fb979",
+    "url": "/quizant/static/js/2.107f0e0b.chunk.js"
+  },
+  {
+    "revision": "f056e6abfd6c65575171",
+    "url": "/quizant/static/js/main.6ce2699e.chunk.js"
+  },
+  {
+    "revision": "94b1f918530024cf01a4",
+    "url": "/quizant/static/js/runtime~main.0f361ec9.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/quizant/static/media/icon-check.4cffa739.svg"
+  }
+]);
